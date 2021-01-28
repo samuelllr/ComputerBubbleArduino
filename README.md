24cf0e669f8cea811e8ccb476c297e8495b7f2b0
Samuelllr <32439511+samuelllr@users.noreply.github.com>
2021-01-28 13:55:38 -0300

    new files

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 Este é um projeto que executa o algoritimo Bubble Soft no arduino, como se fosse um mini-computador. Ele ainda esta em atualização precisa de modificaçoes.
 
-# 𝐵𝑢𝑏𝑏𝑙𝑒 𝑆𝑜𝑓𝑡
+## 𝐵𝑢𝑏𝑏𝑙𝑒 𝑆𝑜𝑓𝑡
 
-Algoritmo organizador de forma crescente ou decrecente.
+Algoritmo que organiza de forma crescente ou decrecente.
 <img src="bs.gif"/>
+Esta imagem mostra como é o funcionamento de um algoritimo Bubble Soft.
+
+
+
---
+++ code/booble-soft/booble-soft.ino
@@ -0,0 +1,150 @@
+
+char numbers[255] = {};
+char n1 , n2;
+byte mode = 0;
+int localArray = -1;
+byte bugInvisivel = 1;
+int pararContagem = -1;
+byte modeFast = 0; 
+int tempDelay = 300;
+
+void setup() {
+
+  Serial.begin( 9600 );
+
+  Serial.println( "---------------- [   ♾ BUBBLE SOFT AGORITHM COMPUTER ♾ ] ----------------" );
+  Serial.println( " ? --> Apresenta o Array. " );
+  Serial.println( " | --> Inicia a organização do Array. " );
+  Serial.println( " ! --> Ativar modo Rápido/Desativar. " );
+  Serial.println( " # --> Limpar Array. " );
+  Serial.println( " " );
+  delay( 2000 );
+  Serial.println( " [ Sistema Pronto para adicionar dados ] " );
+  Serial.println( " " );
+
+}
+
+void loop() {
+
+  char serial = Serial.read( );
+
+  if( mode == 0 ){
+    
+    if( serial > ' ' ){
+
+      if( ( serial != '|' )&&( serial != '?' )&&( serial != ' ' )&&( serial != '!' )&&( serial != '#' ) ){ 
+
+        Serial.println( "  ------[...]------  " );
+        Serial.print( " Adiconado: " );
+        Serial.println( serial );
+
+        localArray++;
+        Serial.print( " Posição a partir do 0: ");
+        Serial.println( localArray );
+        numbers[ localArray ] = serial;
+
+        if( modeFast == 0 ){
+
+          Serial.println( " Array :" );
+    
+          for ( int i = 0 ; i <= localArray ; i++ ){
+            Serial.print( " " );
+            Serial.println( numbers[i] );
+          }
+        
+        }
+
+      }
+
+      switch ( serial ){
+        case '|': mode = 1; Serial.println( " " ); Serial.println( " INICIANDO TRABALHO..." ); delay(2000); break;
+        case '?':
+            Serial.print( " Números no Array: " ); 
+          for( int i = 0; i <= localArray; i++ ){
+            Serial.print( numbers[i] );
+            if( i < localArray ){
+              Serial.print( ", ");
+            }
+          }
+        break;
+        case '!':
+          if( modeFast == 0 ){ 
+            Serial.println( " " );
+            Serial.println( " Modo Rápido ativado. ⚡" );
+            modeFast = 1; 
+            tempDelay = 100; 
+          }else{
+            Serial.println( " " );
+            Serial.println( " Modo Rápido desativado. ↻" );
+            modeFast = 0; 
+            tempDelay = 300; 
+          }
+        break;
+        case '#' :
+           Serial.println( " " );
+           Serial.println( " Array Limpo. ⟵" );
+           memset( numbers , 0 , 255 );
+           localArray = -1;
+        break;
+      }
+
+    }
+    
+  }
+
+  if ( mode == 1 ) start(); 
+
+}
+
+void start(){
+
+  Serial.println( " " );
+  Serial.println( " ORGANIZANDO..." );
+
+  for( int i = 0 ; i < localArray ; i++ ){
+
+     if( i == localArray ) bugInvisivel = 0;
+    
+     n1 = numbers[i];
+     n2 = numbers[ i + bugInvisivel ];
+
+     if(  n1 > n2  ){
+
+        numbers[i] = n2;
+        numbers[ i + bugInvisivel ] = n1;
+
+        pararContagem = -1;
+       
+     }else{
+        pararContagem++;
+     }     
+    
+  }
+
+  if( modeFast == 0 ){
+
+    for( int i = 0; i <= localArray; i++ ){
+       Serial.print( " " );
+       Serial.println( numbers[i] );
+    }
+
+  }
+
+  if( pararContagem > localArray ){ 
+      mode = 0;
+      Serial.println( " Trabalho Terminado." );
+      
+      Serial.println( " Array ORGANIZADO: ");
+      Serial.print( " " );
+      for( int i = 0; i <= localArray; i++ ){
+         Serial.print( numbers[i] );
+         if( i < localArray ){
+            Serial.print( ",");
+         }
+      }
+      
+  }
+
+  delay(tempDelay);
+  
+}
